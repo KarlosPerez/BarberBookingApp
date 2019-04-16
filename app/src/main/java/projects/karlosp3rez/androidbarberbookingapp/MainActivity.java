a1fa86907a62d0abe744c5f38e3721dfcdcf3031
Carlos Perez <legomarth@hotmail.com>
2019-04-16 13:51:14 -0500

    Addes facebook account kit

--- app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/MainActivity.java
+++ app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/MainActivity.java
@@ -1,7 +1,15 @@
 package projects.karlosp3rez.androidbarberbookingapp;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        printKeyHash();
+    }
+
+    private void printKeyHash() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES
+            );
+            for(Signature signature: packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KEYHASH", Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
