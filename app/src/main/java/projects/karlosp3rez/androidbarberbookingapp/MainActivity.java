3df5323c5d3362913638367f32a3a9fcf62d222d
Carlos Perez <legomarth@hotmail.com>
2019-04-16 13:07:47 -0500

    First Commit - Create project

---
+++ app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package projects.karlosp3rez.androidbarberbookingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
