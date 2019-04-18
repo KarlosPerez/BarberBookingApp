d35c89c9d4679914a0c0402d6368c86ff1b75f63
Carlos Perez <legomarth@hotmail.com>
2019-04-17 23:25:30 -0500

    - Config BottomSheetView. - Load fragments to home activity. - Created user model - Update user info and upload to firebase.

--- app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/Activity/HomeActivity.java
+++ app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/Activity/HomeActivity.java
@@ -1,22 +1,162 @@
 package projects.karlosp3rez.androidbarberbookingapp.Activity;
 
+import android.app.AlertDialog;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import dmax.dialog.SpotsDialog;
+import projects.karlosp3rez.androidbarberbookingapp.Common.Common;
+import projects.karlosp3rez.androidbarberbookingapp.Fragments.HomeFragment;
+import projects.karlosp3rez.androidbarberbookingapp.Fragments.ShoppingFragment;
+import projects.karlosp3rez.androidbarberbookingapp.Model.User;
 import projects.karlosp3rez.androidbarberbookingapp.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
+    
+    BottomSheetDialog bottomSheetDialog;
+    CollectionReference userRef;
+    AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(HomeActivity.this);
+
+        //Init
+        userRef = FirebaseFirestore.getInstance().collection("User");
+        dialog = new SpotsDialog.Builder().setContext(this).setCancelable(false).build();
+
+        //Check intent, if is login = true, enable full access
+        // If is login = false, just let user around shopping to view
+        if(getIntent() != null) {
+            boolean isLogin = getIntent().getBooleanExtra(Common.IS_LOGIN, false);
+            if(isLogin) {
+                dialog.show();
+                //Check if user exists
+                AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                    @Override
+                    public void onSuccess(final Account account) {
+                        if(account != null) {
+                            DocumentReference currentUser = userRef.document(account.getPhoneNumber().toString());
+                            currentUser.get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if(task.isSuccessful()) {
+                                                DocumentSnapshot userSnapShot = task.getResult();
+                                                if(!userSnapShot.exists()) {
+                                                    showUpdateDialog(account.getPhoneNumber().toString());
+                                                }
+                                                if(dialog.isShowing())
+                                                    dialog.dismiss();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onError(AccountKitError accountKitError) {
+                        Toast.makeText(HomeActivity.this, ""+accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+
+        //view
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.action_home)
+                    fragment = new HomeFragment();
+                else if(menuItem.getItemId() == R.id.action_shopping)
+                    fragment = new ShoppingFragment();
+                return loadFragment(fragment);
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    private void showUpdateDialog(final String phoneNumber) {
+        //Init dialog
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setTitle("One More Step!");
+        bottomSheetDialog.setCanceledOnTouchOutside(false);
+        bottomSheetDialog.setCancelable(false);
+        View sheetView = getLayoutInflater().inflate(R.layout.layout_update_information, null);
+        Button btn_update = sheetView.findViewById(R.id.btn_update);
+        final TextInputEditText txt_name = sheetView.findViewById(R.id.txt_name);
+        final TextInputEditText txt_address = sheetView.findViewById(R.id.txt_address);
+
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!dialog.isShowing())
+                    dialog.show();
+                User user = new User(txt_name.getText().toString(),
+                        txt_address.getText().toString(),
+                        phoneNumber);
+                userRef.document(phoneNumber)
+                        .set(user)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                bottomSheetDialog.dismiss();
+                                if(dialog.isShowing())
+                                    dialog.dismiss();
+                                Toast.makeText(HomeActivity.this, "Thank you", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if(dialog.isShowing())
+                            dialog.dismiss();
+                        bottomSheetDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        bottomSheetDialog.setContentView(sheetView);
+        bottomSheetDialog.show();
     }
 }
---
+++ app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/Model/User.java
@@ -0,0 +1,40 @@
+package projects.karlosp3rez.androidbarberbookingapp.Model;
+
+public class User {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public User() {
+    }
+
+    public User(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
