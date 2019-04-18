7075bef6dbdf61ee25dfae65ee4d0591b5b82d20
Carlos Perez <legomarth@hotmail.com>
2019-04-17 22:10:11 -0500

    - Created bottom navigation view. - Created menu items Home, Shopping. - Created fragments Home, Shopping.

--- app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/Activity/HomeActivity.java
+++ app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/Activity/HomeActivity.java
@@ -1,8 +1,15 @@
 package projects.karlosp3rez.androidbarberbookingapp.Activity;
 
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import projects.karlosp3rez.androidbarberbookingapp.R;
 
 public class HomeActivity extends AppCompatActivity {
