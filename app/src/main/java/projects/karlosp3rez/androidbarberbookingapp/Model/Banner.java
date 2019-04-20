7e62fb5860fcf6c554403c3d71a887d599600db8
Carlos Perez <legomarth@hotmail.com>
2019-04-19 21:30:35 -0500

    Implement ImageLoadingService to load images with picasso

---
+++ app/src/main/java/projects/karlosp3rez/androidbarberbookingapp/Model/Banner.java
@@ -0,0 +1,21 @@
+package projects.karlosp3rez.androidbarberbookingapp.Model;
+
+public class Banner {
+    //Lookbook and banner is same
+    private String image;
+
+    public Banner() {
+    }
+
+    public Banner(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
